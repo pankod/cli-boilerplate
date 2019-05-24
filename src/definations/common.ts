fce8bd73981c1ad504d197ec001956d71d4ff30f
Omer Faruk APLAK <omeraplak@gmail.com>
2019-05-24 14:35:43 +0300

    added editorconfig

--- src/definations/common.ts
+++ src/definations/common.ts
@@ -2,85 +2,85 @@ import * as inquirer from 'inquirer';
 import { Helper } from '../definations/helper';
 import { Config } from '../../config';
 
-export module Common{
+export module Common {
 
-    export const commonValues = {
-        st_message: 'Enter file name',
-        st_name: 'fileName',
-        st_type: 'input',
-        ac_message: 'Enter file name',
-        ac_name: 'fileName',
-        ac_type: 'input',
-    }
+	export const commonValues = {
+		st_message: 'Enter file name',
+		st_name: 'fileName',
+		st_type: 'input',
+		ac_message: 'Enter file name',
+		ac_name: 'fileName',
+		ac_type: 'input'
+	}
 
-    export const simpleTextQuestions = [
-        {
-            message: Common.commonValues.st_message,
-            name: Common.commonValues.st_name,
-            type: Common.commonValues.st_type,
-            validate(val: string): string | boolean {
-                return Common.validate(val);
-            }
-        },
-        {
-            default: false,
-            message: 'Do you want to add file name into file content ?',
-            name: 'isFileNameAdd',
-            type: 'confirm'
-        }
-    ];
+	export const simpleTextQuestions = [
+		{
+			message: Common.commonValues.st_message,
+			name: Common.commonValues.st_name,
+			type: Common.commonValues.st_type,
+			validate(val: string): string | boolean {
+				return Common.validate(val);
+			}
+		},
+		{
+			default: false,
+			message: 'Do you want to add file name into file content ?',
+			name: 'isFileNameAdd',
+			type: 'confirm'
+		}
+	];
 
-    export const addCollectionQuestions = [
-        {
-            message: Common.commonValues.ac_message,
-            name: Common.commonValues.ac_message,
-            type: Common.commonValues.ac_message,
-            validate(val: string): string | boolean {
-                return Common.validate(val);
-            }
-        },
-        {
-            choices: [
-                new inquirer.Separator(),
-                {
-                    name: 'Yes, I want to enter custom name file.',
-                    value: true
-                },
-                {
-                    name: 'No, use default file name.',
-                    value: false
-                }
-            ],
-            default: false,
-            message: 'Would you like save file to collection.ts with a custom name?',
-            name: 'isCustomFileName',
-            type: 'list'
-        },
-        {
-            message: 'Enter custom name',
-            name: 'customFileName',
-            type: 'input',
-            when: ({ isCustomFileName }) => isCustomFileName
-        }
-    ];
+	export const addCollectionQuestions = [
+		{
+			message: Common.commonValues.ac_message,
+			name: Common.commonValues.ac_message,
+			type: Common.commonValues.ac_message,
+			validate(val: string): string | boolean {
+				return Common.validate(val);
+			}
+		},
+		{
+			choices: [
+				new inquirer.Separator(),
+				{
+					name: 'Yes, I want to enter custom name file.',
+					value: true
+				},
+				{
+					name: 'No, use default file name.',
+					value: false
+				}
+			],
+			default: false,
+			message: 'Would you like save file to collection.ts with a custom name?',
+			name: 'isCustomFileName',
+			type: 'list'
+		},
+		{
+			message: 'Enter custom name',
+			name: 'customFileName',
+			type: 'input',
+			when: ({ isCustomFileName }) => isCustomFileName
+		}
+	];
 
-    export const validate = (val: string): string | boolean => {
-        if (val.length) {
-            if (
-                Helper.isAlreadyExist(
-                    Config.filesDir,
-                    val,
-                    true
-                )
-            ) {
-                return 'This file name already used before, enter new name.';
-            }
+	export const validate = (val: string): string | boolean => {
+		if (val.length) {
+			if (
+				Helper.isAlreadyExist(
+					Config.filesDir,
+					val,
+					true
+				)
+			) {
+				return 'This file name already used before, enter new name.';
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        return 'Can not be empty';
-    }
+		return 'Can not be empty';
+	}
 }
 
 export default Common;
